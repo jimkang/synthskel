84184d52b28ecd2059dc2110a8c3dffc0dca93d8
Jim Kang <jimkang@gmail.com>
2023-09-23 14:21:59 -0400

    Add constructor for tonality diamond.

--- tonality-diamond.js
+++ tonality-diamond.js
@@ -1,38 +1,47 @@
-import { diamondLimit } from './consts';
-const diamondSideLength = Math.ceil(diamondLimit/2);
+export function TonalityDiamond({ diamondLimit }) {
+  const diamondSideLength = Math.ceil(diamondLimit / 2);
 
-var diamondRatios = [];
+  var diamondRatios = [];
 
-for (let row = 0; row < diamondSideLength; ++row) {
-  const denominator = diamondLimit - row;
-  for (let col = 0; col < diamondSideLength; ++col) {
-    const numerator = diamondLimit - col;
-    diamondRatios.push({ numerator, denominator });
+  for (let row = 0; row < diamondSideLength; ++row) {
+    const denominator = diamondLimit - row;
+    for (let col = 0; col < diamondSideLength; ++col) {
+      const numerator = diamondLimit - col;
+      diamondRatios.push({ numerator, denominator });
+    }
   }
-}
 
-diamondRatios = diamondRatios.map(factorDown);
-diamondRatios.sort((a, b) => a.denominator - b.denominator);
+  diamondRatios = diamondRatios.map(factorDown);
+  diamondRatios.sort((a, b) => a.denominator - b.denominator);
 
-// Dedupe.
-for (let i = diamondRatios.length - 1; i > 0; --i) {
-  let ratio = diamondRatios[i];
-  let prevRatio = diamondRatios[i - 1];
-  if (ratio.numerator === prevRatio.numerator && ratio.denominator === prevRatio.denominator) {
-    diamondRatios.splice(i, 1);
+  // Dedupe.
+  for (let i = diamondRatios.length - 1; i > 0; --i) {
+    let ratio = diamondRatios[i];
+    let prevRatio = diamondRatios[i - 1];
+    if (
+      ratio.numerator === prevRatio.numerator &&
+      ratio.denominator === prevRatio.denominator
+    ) {
+      diamondRatios.splice(i, 1);
+    }
   }
+
+  var tonalityDiamondPitches = [1, 2].concat(
+    diamondRatios
+      .slice(1)
+      .map(({ numerator, denominator }) => numerator / denominator)
+  );
+
+  return { pitches: tonalityDiamondPitches, ratios: diamondRatios };
 }
-
-console.log(diamondRatios);
-
-export var tonalityDiamondPitches = [1, 2].concat(diamondRatios.slice(1).map(({ numerator, denominator }) => numerator/denominator));
-
-console.log(tonalityDiamondPitches);
 
 function factorDown({ numerator, denominator }) {
   var commonFactor = findCommonFactor(numerator, denominator);
   if (commonFactor) {
-    return { numerator: numerator/commonFactor, denominator: denominator/commonFactor };
+    return {
+      numerator: numerator / commonFactor,
+      denominator: denominator / commonFactor,
+    };
   }
   return { numerator, denominator };
 }
@@ -40,9 +49,8 @@ function factorDown({ numerator, denominator }) {
 // Assumes whole numbers for a and b.
 function findCommonFactor(a, b) {
   for (let factor = a; factor > 0; --factor) {
-    if (a/factor % 1 === 0 && b/factor % 1 === 0) {
+    if ((a / factor) % 1 === 0 && (b / factor) % 1 === 0) {
       return factor;
     }
   }
 }
-
